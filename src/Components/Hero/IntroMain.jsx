a0cefe9e3328159de2c974d24fe2fb864afc789f
Mohit1x <mohitvyas746@gmail.com>
2024-09-13 13:23:41 +0530

    fixed about component errror

--- src/Components/Hero/IntroMain.jsx
+++ src/Components/Hero/IntroMain.jsx
@@ -1,7 +1,7 @@
 import { useState } from "react";
-import AboutComponent from "./aboutComponent";
 import ExperienceComponent from "./ExperienceComponent";
 import Recommended from "./Recommended";
+import AboutComponent from "./aboutComponent";
 
 const IntroMain = () => {
   const [tab, setTab] = useState("about");
